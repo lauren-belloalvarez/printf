d00f98fd1c9237e37d3169ff8f881efa8b59d0c3
lauren-belloalvarez <lauren2104ba@yahoo.com>
2023-08-22 15:55:49 -0700

    Add get-handler function

---
+++ get_handler.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * get_handler - finds specifier and relavent conversion function
+ *
+ * @spec: specifier
+ *
+ * Return: formatted item
+ */
+
+int (*get_handler(char spec))(va_list)
+{
+	unsigned long int i;
+
+	fmt_t arr[] = {
+	{'c', print_char},
+	{'s', print_str},
+	{'%', print_percent},
+	{'i', print_int},
+	{'d', print_int}
+	};
+
+	for (i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		if (arr[i].spec == spec)
+			return (arr[i].hdler);
+	}
+	return (NULL); /*if spec not found*/
+}
